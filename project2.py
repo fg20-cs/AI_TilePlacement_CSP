529c90c3f444099fde8719979076a39548e5ccd6
fidanguliyeva <f2guliyeva@gmail.com>
2024-03-02 00:43:41 +0400

    working state with MRV heuristic

--- project2.py
+++ project2.py
@@ -1,4 +1,5 @@
 import sys
+from print import *
 
 # getting file name from the command line
 input_args = len(sys.argv)
@@ -55,7 +56,7 @@ tiles = {}
 bushes_target = {}
 
 extract_data_from_file(file_name, landscape_areas, tiles, bushes_target)
- 
+
 # fucntion to apply FULL_BLOCK to a 4x4 area
 # returns a dictionary with the count of visible bushes of each type
 def Full_block(area):
@@ -118,3 +119,81 @@ def apply(func, area):
 
 # keep all the possible tile assignments in the list
 functions = [L_shape_1, L_shape_2, L_shape_3, L_shape_4, Outer_block, Full_block]
+
+# function to check 3 conditions: 
+def check(current_state, path):
+
+  #checking if the tile count is not exceeding the available number of tiles
+  count_L = path.count('L_shape_1') + path.count('L_shape_2') + path.count('L_shape_3') + path.count('L_shape_4')
+  count_full = path.count('Full_block')
+  count_out = path.count('Outer_block')
+  if (count_L > tiles['EL_SHAPE']) or (count_full > tiles['FULL_BLOCK']) or (count_out > tiles['OUTER_BOUNDARY']):
+    return 'used too many tiles'
+
+  #checking the difference between visible bushes in current state and the goal state
+  dist = {key: bushes_target[key] - current_state[key] for key in bushes_target if key in current_state}
+  goal = True
+  for value in dist.values():
+    if value != 0:
+      goal = False
+      break
+  
+  # reached the goal state
+  if goal:
+    return 'end'
+  
+  # uncovered too many bushes
+  if min(dist.values()) < 0:
+    return 'too many visible bushes'
+    
+  return None
+
+# recursive function that checks constraints and applies MRV heuristics
+def csp_with_MRV(current_index, current_sum, path, functions, landscape):  
+
+  # check if reached the final state
+  if current_index == len(landscape):
+    if check(current_sum, path) == 'end':            
+      return True
+    else: return False
+  
+  #applying MRV heuristic
+  else:
+    all_functions = [f for f in functions]
+    all_functions = sorted(all_functions, key=lambda f: len([v for v in f(landscape[current_index]).values() if v > 0]))
+    
+    for func in all_functions:
+      path.append(func.__name__)
+      applied_area = apply(func, landscape[current_index])
+        
+      current_bushes = {}
+      for key in applied_area:
+        if key in current_sum:
+          current_bushes[key] = current_sum[key] + applied_area[key]
+          
+      # Check if any constraint is violated
+      checks = check(current_bushes, path)
+      if checks == 'too many visible bushes' or checks == 'used too many tiles':
+        path.pop()
+        continue
+                        
+      solution = csp_with_MRV(current_index + 1, current_bushes, path, functions, landscape)
+              
+      if solution: return True
+      else: path.pop()
+
+    return False
+
+path = []
+solution = csp_with_MRV(0, {1:0, 2:0, 3:0, 4:0}, path, functions, landscape_areas)
+  
+apply_path_to_landscape(landscape_areas, path)
+print_landscape_with_blocks(landscape_areas)    
+
+if solution:
+    print("Final Path:")
+    print(path)
+else:
+    print("No path found.")
+
+print("")
